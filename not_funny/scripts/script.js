f4d766f05736dec08921eaf19b68cef183a46e05
r3nder <noremorse@gmail.com>
2020-05-18 02:11:07 -0300

    Add files via upload

---
+++ not_funny/scripts/script.js
@@ -0,0 +1,29 @@
+var Scene = require('Scene')
+var D = require('Diagnostics')
+var Reactive = require('Reactive')
+
+var plane0 = Scene.root.findFirst('plane0')
+var plane1 = Scene.root.findFirst('plane1')
+//var text0 = Scene.root.findFirst('text0')
+
+// // target to track distance to
+//var planePosition = Reactive.point(plane0.transform.x.pinLastValue(), plane0.transform.y.pinLastValue(), plane0.transform.z.pinLastValue())
+// D.watch('plane.x', planePosition.x)
+// D.watch('plane.y', planePosition.y)
+// D.watch('plane.z', planePosition.z)
+
+// var pixelPoint = Reactive.point2d(Reactive.val(0), Reactive.val(0))
+// // unproject a short distance. zero doesn't work
+// var camPosition = Scene.unprojectWithDepth(pixelPoint, 0.01)
+// D.watch('cam.x', camPosition.x)
+// D.watch('cam.y', camPosition.y)
+// D.watch('cam.z', camPosition.z)
+
+// var distance = camPosition.distance(planePosition).round()
+// D.watch('distance', distance)
+
+// // display distance
+// text0.text = distance.toString()
+
+// // unhide if within a distance
+// plane1.hidden = distance.gt(300)
